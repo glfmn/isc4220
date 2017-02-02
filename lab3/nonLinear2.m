c8ed494539b9b84b3de17194a14ecc68c33a8b23
Gwen Lofman <Gwen@Lofman.co>
2017-02-02 16:27:24 -0500

    Solve problem 1: Nth Roots

--- lab3/nonLinear2.m
+++ lab3/nonLinear2.m
@@ -7,6 +7,33 @@
 %%%
 % *ISC4220C*
 %%
+%% Nth roots of real numbers
+% The roots of real numbers are easy to approximate with the formula:
+%
+% $$ f{x} = x^{n} - a $$
+%
+% Using newtons method, we find the roots of this formula to find the
+% square root of two and the fifth root of five to at least a precision of
+% 10^-4.
+
+clear
+
+f  = @(x) x^2 - 2;
+df = @(x) 2*x;
+
+root = newton(f, df, 1, 10^-4); % approximate value of 2^(1/2)
+true_err = abs(2^(1/2)-root);
+fprintf('\napproximation of 2^(1/2) =\n\n    %g\n\n\n',root);
+fprintf('true error =\n\n    %g\n\n\n',true_err);
+
+f  = @(x) x^5 - 5;
+df = @(x) 5*x.^4;
+
+root = newton(f, df, 1, 10^-4); % approximate value of 5^(1/5)
+true_err = abs(5^(1/5)-root);
+fprintf('approximation of 5^(1/5) =\n\n    %g\n\n\n',root);
+fprintf('true error =\n\n    %g\n\n\n',true_err);
+
 %% Calculating $$ \pi $$
 % Using John Machin's identity, we can solve for $$\pi$$.
 %
