f8d7aac08bfbd81cbe1f6a861cdc8cf26f79c8c3
Gwen Lofman <Gwen@Lofman.co>
2017-02-23 23:24:30 -0500

    Implement bisection 1D optimization problem

--- lab6/optima.m
+++ lab6/optima.m
@@ -8,16 +8,28 @@
 %% Growth Rate
 clear
 
-% Implement the growth rate function as defined in the problem
+% Implement the growth rate function as defined in the problem:
 g = @(c) 2.*c ./ (4 + 0.8.*c + c.^2 + 0.2.*c.^3);
+gp = @(c) 2*(-0.4 * c.^3 - c.^2 + 4) ./ (4 + 0.8.*c + c.^2 + 0.2.*c.^3).^2;
 
 % Set x values for the plot
-x     = 0:0.1:3;
-x_max = golden(g,1,2)
+x    = 0:0.1:3;
+xMax = golden(g,1,2);
+xBis = bisection(gp,1,2);
 
-% Plot the function and the maximum to visually inspect the result
+% Plot the function and the maximum to visually inspect the results, and
+% reveal the similarities between golden section search for maxima and the
+% bisection method for root finding.
 figure(1);
-plot(x,g(x),x_max,g(x_max),'r*')
+hold on
+
+plot(x,g(x))             % Plot g(x) vs its maximum to inspect result
+plot(xMax,g(xMax),'r*')
+
+plot(x,gp(x))            % Plot g'(x) against its root
+plot(xBis,gp(xBis),'ro')
+
+hold off
 
 %% RosenBrock
 clear
