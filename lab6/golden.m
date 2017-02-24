c62d7af56e3d283b9629a7517715e4369c62de1f
Gwen Lofman <Gwen@Lofman.co>
2017-02-23 23:25:39 -0500

    Refactor variable names to eliminate confusion
    Previosly, x1 was easy to confuse with xl; changing xl to a and
    xu to b remove ambiguity between x1 and xl, and help enforce the
    mental seperation between the outer and inner values of the
    golden secition.

--- lab6/golden.m
+++ lab6/golden.m
@@ -1,4 +1,4 @@
-function [ max, its, xl, xu ] = golden( func, xl, xu, maxIt, converge )
+function [ max, its, a, b ] = golden( func, a, b, maxIt, converge )
 %GOLDEN performs the golden section search to find maxima of a 2D function
 %   over a range.  As a closed method it is guaranteed to converge as long
 %   as only one maximum exists in the range.
@@ -26,17 +26,17 @@ function [ max, its, xl, xu ] = golden( func, xl, xu, maxIt, converge )
 %%% Assign default arguments
 
 if nargin<4, maxIt    = 50;                        end
-if nargin<5, converge = @(xl,xu) abs(xu-xl)<10^-6; end
+if nargin<5, converge = @(xl,xu) abs(xu-xl)<10^-8; end
 
 %%% Define useful values
 
 R = (sqrt(5)-1)/2; % golden ratio
-d = R * (xu-xl);   % size of the golden section
+d = R * (b-a);   % size of the golden section
 
 its       = 0;
 converged = false;
-x1 = xl + d;
-x2 = xu + d;
+x1 = a + d;
+x2 = b + d;
 f1 = func(x1);
 f2 = func(x2);
 
@@ -45,24 +45,24 @@ f2 = func(x2);
 for its=1:maxIt
     d = R * d;
     if (f1 < f2)
-        xl = x2;
+        a = x2;
         x2 = x1;
-        x1 = xl + d;
+        x1 = a + d;
         f2 = f1;
         f1 = func(x1);
     else
-        xu = x1;
+        b = x1;
         x1 = x2;
-        x2 = xu - d;
+        x2 = b - d;
         f1 = f2;
         f2 = func(x2);
     end
-    if converge(xu,xl), converged = true; break; end;
+    if converge(b,a), converged = true; break; end;
 end
 
 if ~converged, warning('Function terminated without converging.'); end
 
-max = xl;
+max = a;
 
 end
 
