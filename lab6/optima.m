ddec30a262fe1f6df55eb7e3f0c9013252d7e9d3
Gwen Lofman <Gwen@Lofman.co>
2017-02-27 00:24:41 -0500

    Finish 2D rosenbrock optimization problem
    Includes changes to comments to support report syntax, and updates
    plots with titles, and prints information about the results of
    the functions.

--- lab6/optima.m
+++ lab6/optima.m
@@ -45,17 +45,49 @@ plot(xBis,gp(xBis),'ro');
 hold off;
 
 %% RosenBrock
+% The Rosenbrock function is often used to test the performance of
+% optimization algorithms, because its global minimum, which occurs at
+% (1,1), lies within a narrow parabolic valley.
+%
+%  f(x1,x2) = (1 − x1)^2 + 100(x2 − x1^2)^2
 clear
 
+%%%
 % Implement the rosenbrock function as defined in the problem
-r = @(x_1, x_2) (1 - x_1).^2 + 100*(x_2 - x_1.^2).^2;
+r  = @(x1,x2) (1 - x1).^2 + 100*(x2 - x1.^2).^2;
+r_ = @(x) r(x(1),x(2)); % r as a function that accepts a vector
 
+%%%
+% Implement gradient function
+drdx1 = @(x) -2 + 2*x(1)-400*x(1).*(x(2) - x(1).^2);
+drdx2 = @(x) 200*(x(2) - x(1).^2);
+gr    = @(x) [ drdx1(x); drdx2(x) ];
+
+%%%
 % Calculate values of Z to enable plotting the function
-range = 2;
-step  = 0.1;
+range = 1.5;
+step  = range/15;
 [X,Y] = meshgrid(-range:step:range);
 Z     = size(X);
 Z     = r(X,Y);
 
+%%%
+% Calculate the minimum
+[min, fmin, k] = bfgs(r_,gr,[-1;1],50);
+x = min(1); y = min(2);
+fprintf('Min %g at (%g,%g)\n',fmin,x,y);
+
+%%%
+% Plot the function and the minimum to visually inspect the results.
 figure(2);
+title('Minimum of the rosenbrock function');
+view(-165,65);
+hold on;
 surf(X,Y,Z);
+plot3(min(1),min(2),fmin,'ro');
+hold off;
+
+%%
+% After 50 iterations bfgs() does not converge on a solution; it takes 122
+% iterations for bfgs() to converge and find the global minimum of the
+% rosenbrock function at (1,1)
