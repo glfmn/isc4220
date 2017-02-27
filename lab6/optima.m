bae2ccc8d51c73fdc860ea90f3a49168407e8f6e
Gwen Lofman <Gwen@Lofman.co>
2017-02-26 23:26:49 -0500

    Finalize comments for growth rate problem
    Add publishing formatting to comments in growth rate problem to
    make the output of the publish function more readable for this
    section, and update graphs with titles.

--- lab6/optima.m
+++ lab6/optima.m
@@ -1,35 +1,48 @@
-%% LU Decomposition and Gauss-Seidel
-% 
+%% Optimization: Lab 6
 %%%
 % *Gwen Zapata*
 %%%
 % *ISC4220C*
 %%
 %% Growth Rate
+% The growth rate, g , of yeast used to produce an antibiotic depends on the
+% concentration of the food c as:
+%
+%   g(c) = 2c / (4 + 0.8c + c^2 + 0.2c^3)
+%
+% The units of g and c, in the above equation, are per day and mg/L,
+% respectively. The growth rate is small when c is small (as the yeast
+% starve), and when c is very large (toxicity effects). We want to find the
+% c at which g is maximum.
 clear
 
+%%%
 % Implement the growth rate function as defined in the problem:
 g = @(c) 2.*c ./ (4 + 0.8.*c + c.^2 + 0.2.*c.^3);
 gp = @(c) 2*(-0.4 * c.^3 - c.^2 + 4) ./ (4 + 0.8.*c + c.^2 + 0.2.*c.^3).^2;
 
+%%%
 % Set x values for the plot
 x    = 0:0.1:3;
 xMax = golden(g,1,2);
 xBis = bisection(gp,1,2);
 
+%%%
 % Plot the function and the maximum to visually inspect the results, and
 % reveal the similarities between golden section search for maxima and the
 % bisection method for root finding.
 figure(1);
-hold on
-
-plot(x,g(x))             % Plot g(x) vs its maximum to inspect result
-plot(xMax,g(xMax),'r*')
-
-plot(x,gp(x))            % Plot g'(x) against its root
-plot(xBis,gp(xBis),'ro')
-
-hold off
+hold on;
+title('Maximum growth rate');
+%%%
+% Plot g(x) vs its maximum to inspect result
+plot(x,g(x));
+plot(xMax,g(xMax),'r*');
+%%%
+% Plot g'(x) against its root
+plot(x,gp(x));
+plot(xBis,gp(xBis),'ro');
+hold off;
 
 %% RosenBrock
 clear
