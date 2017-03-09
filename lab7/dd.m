3f7646b82e04c6368405cc80435edd43fbb74e3d
Gwen Lofman <Gwen@Lofman.co>
2017-03-09 15:17:20 -0500

    Fix bug in divided difference calculation
    Correct index names to better reflect the actual purpose, and
    fixes a bug that would calculate the wrong divided difference
    table because it would divide by a wrong x value.

--- lab7/dd.m
+++ lab7/dd.m
@@ -1,7 +1,17 @@
 function [ y, B ] = dd( xs, fs, x, B )
 %DD is the divided difference interpolation of a set of points x and f(x)
 %
-%   DD(X, F)
+%   DD(X, F, x) interpolates the function f from the points (x0,f0) where
+%   X: is a vector
+%   F: is a vector of length(X)
+%   x: is a vector of points to interpolate
+%   DD(X, F, x, B) interpolates f from the points (x0,f0) where
+%   B: is a divided difference matrix
+%   X: is a vector of new coordinates to calculate divided differences for
+%   F: corresponds to X and has length(X)
+%   This variant allows additional data points to improve the interpolation
+%   of the function with a lower complexity, in this case O(n) instead of
+%   O(n^2).
 
 % Either calculate new matrix of divided differences, or update the
 % matrix passed into the function
@@ -30,14 +40,15 @@ function [B] = b(f, x, B)
 %       b[x0..xn] = b[0..n-1] - b[1..n] / x0 - xn
 %       b[x0..x1] = f(x0)     - f(x1)   / x0 - x1
 
-if nargin < 3, n = numel(f)-1; s=1; else n = numel(f)-1; s=size(B,1); end
+if nargin < 3, n = numel(f)-1; s=1; else s=size(B,1); n=s+numel(f)-1; end
+if s==n,       warning('Will not update B matrix');                   end
 
-for row=s:n
-    B(row,1) = (f(row+1) - f(row)) / (x(row+1) - x(row));
-    if row ~= 1;
-        for col = 2:size(B,1)
-            row_ = row-col+1;
-            B(row_,col) = (B(row_,col-1)-B(row_+1,col-1)) / (x(1)-x(col));
+for r=s:n
+    B(r,1) = (f(r+1) - f(r)) / (x(r+1) - x(r));
+    if r ~= 1
+        for c = 2:size(B,1)
+            r_ = r-c+1;
+            B(r_,c) = (B(r_,c-1)-B(r_+1,c-1)) / (x(r_)-x(r+1));
         end
     end
 end
