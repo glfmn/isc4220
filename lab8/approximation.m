9083dc7a5c71ec6597d8aa328c92fac97ce60ab2
Gwen Lofman <Gwen@Lofman.co>
2017-04-18 22:26:22 -0400

    Implement linear least squares in problem 2
    Part 1 of the exam question

--- lab8/approximation.m
+++ lab8/approximation.m
@@ -33,3 +33,41 @@ rank = (game_matrix'*game_matrix) \ (game_matrix'*differences);
 
 fprintf('T1\tT2\tT3\tT4\n');
 fprintf('%g\t%g\t%g\t%g\n',rank(1),rank(2),rank(3),rank(4));
+
+%% 2014 Exam Problem
+%
+clear
+
+% Data from problem specification
+data = [
+    0.00     2.10
+    0.25     3.70
+    0.50     6.26
+    0.75    10.03
+    1.00    16.31
+];
+
+xs = data(:,1);
+ys = data(:,2);
+
+%%%
+% Using linear least squares, we can take the model
+m1 = @(a,b,t) a.*t + b.*exp(2*t);
+%%%
+% and fit the parameters $$ a_1 $$ and $$ b_1 $$ to our data set to minimize
+% the difference between the model and data where a and b are the linearly
+% independent parameters to minimize using lls.
+
+% Create a matrix seperating the linear terms of the model
+A = [m1(1,0,xs) m1(0,1,xs)];
+
+% Calculate coefficients with linear least squares
+c = (A'*A)  \ (A'*ys);
+
+ts = linspace(0,1,50);
+
+% Plot results to inspect fitting
+figure(1);
+plot(xs,ys,'ro',ts,m1(c(1),c(2),ts),'b');
+title('Model 1: Linear Least Squares');
+legend('data','fit');
