6b3ecd2b4a3eb5332af983c7c1170ab1a5987a4a
Gwen Lofman <Gwen@Lofman.co>
2017-04-25 13:03:45 -0400

    Scaffold lab document with description, sections
    Includes synopsis of the lab file, and a section for each part of
    the problem.

---
+++ lab9/differentiation.m
@@ -0,0 +1,11 @@
+%% Numerical Differentiation
+% Gwen Zapata
+%%%
+% ISC4220
+%%%
+% Numerical differentiation lab, examining the truncation error and and
+% round-off error for the fourth-order central difference formula for
+% estimating the second derivative.
+%% Approximating total error
+
+%% Investigating error properties
