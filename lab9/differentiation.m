5b251bf1c8b2707d97244bf41ae0a6bf14e16f6c
Gwen Lofman <Gwen@Lofman.co>
2017-04-26 16:47:42 -0400

    Complete part 2 investigating error properties

--- lab9/differentiation.m
+++ lab9/differentiation.m
@@ -41,3 +41,59 @@ d = @(x,h)...
 absErr = @(x,h) abs(exp(x)-d(x,h));
 
 %% Investigating error properties
+% Since we have an easy analytial solution for second derivative, we can
+% take the absolute error between the second derivative and the central
+% difference formula, and just use a method like the golden section search
+% to minimize that error.
+
+[minErr,h] = golden(@(h)absErr(0,h),0,1,100,10e-16);
+
+%%%
+% Comparing h values of the derivative at $x = 0$ to the expression for the
+% total error, and plotting the optimal h and the minimum error we have:
+
+figure(1);
+hs = logspace(-10,0,100); % logarithmically equispaced domain
+loglog( hs,total(0,hs)...
+      , hs,absErr(0,hs),'m:'...
+      , h,minErr,'ro');
+legend( 'error expression'...
+      , 'absolute error'...
+      , sprintf('optimal h of 10^{%g}',log10(h)));
+text(h/2,minErr,sprintf('min err %g',minErr)...
+    ,'HorizontalAlignment','right');
+ylabel('error');
+xlabel('h');
+title('Order of total error at x=0');
+
+%%
+
+figure(2);
+xs = 0:0.005:1;
+semilogy( xs,absErr(xs,h),     'm-'...
+        , xs,absErr(xs,h/10),  'r--'...
+        , xs,absErr(xs,h/100), 'b--'...
+        , xs,absErr(xs,h*10),  'c--'...
+        , xs,absErr(xs,h/1000),'g--');
+legend( sprintf('optimal h of 10^{%g}',log10(h))...
+      , sprintf('h of 10^{%g}', log10(h/10))...
+      , sprintf('h of 10^{%g}', log10(h/100))...
+      , sprintf('h of 10^{%g}', log10(h*10))...
+      , sprintf('h of 10^{%g}', log10(h/1000)));
+ylabel('absolute error');
+xlabel('x');
+title('Absolute error for different h');
+
+%%
+
+figure(3);
+hold on;
+title('Analytical derivative and central difference approximations');
+plot(xs,exp(xs),'c-', 'LineWidth', 2);
+plot( xs,d(xs,h),       'r--'...
+    , xs,d(xs,h/100000),'m:');
+legend( 'true derivative'...
+      , sprintf('optimal h of 10^{-%g}',log10(h))...
+      , sprintf('h of 10^{-%g}'        ,log10(h/100000)));
+title('Analytical and central difference approximations');
+hold off;
